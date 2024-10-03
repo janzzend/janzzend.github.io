d95d7df93075f94172fc42ce062790d43f16770a
Janzzen Matthew De Jesus <janzzen.dejesus@gmail.com>
2024-10-04 04:00:20 +0800

    svelte navbar

--- src/lib/data/navbar.ts
+++ src/lib/data/navbar.ts
@@ -3,5 +3,5 @@ export const items = [
 	{ title: 'Projects', to: '/projects', icon: 'i-carbon-cube' },
 	{ title: 'Experience', to: '/experience', icon: 'i-carbon-development' },
 	{ title: 'Education', to: '/education', icon: 'i-carbon-education' },
-	{ title: 'Resumé', to: 'https://drive.google.com/file/d/1gW9o9hG3pzH9BDOL316gBsT6LZWkwpg6/view', icon: 'i-carbon-result' }
+	{ title: 'Resumé', to: '/resume', icon: 'i-carbon-result' }
 ] as const;
