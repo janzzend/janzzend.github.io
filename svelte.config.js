e4cda6f370797ca897ed551054297b5a12929421
Janzzen Matthew De Jesus <janzzen.dejesus@gmail.com>
2024-10-04 03:57:44 +0800

    svelte config

--- svelte.config.js
+++ svelte.config.js
@@ -1,7 +1,7 @@
 import adapter from '@sveltejs/adapter-static';
 import { vitePreprocess } from '@sveltejs/kit/vite';
 
-const base = '/janzzend-profile-v2';
+const base = '/janzzend.github.io';
 
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
